a79cbe41ef985a22e98f759a06d193792ffff75f
inegg <inegg.apps@gmail.com>
2020-06-26 22:14:23 +0900

    flex -> float로 레이아웃 css 수정, 카테고리 스크롤 jquery 수정, mvc-config.xml에 로그아웃 시에도 접근할 수 있는 자원 예외 추가

--- src/main/webapp/resource/js/main/main.js
+++ src/main/webapp/resource/js/main/main.js
@@ -12,14 +12,16 @@ $(function () {
 function scrollCategory() {
   const $viewer = $(".categoryViewer");
   const $content = $(".categoryContent");
-  const itemWidth = 100;
+  const length = $content.find("li").length;
+  const itemWidth = 100; //여백 포함 한 아이템이 차지하는 공간 가로 너비
+  const endItemPos = itemWidth * length; // 맨 끝 아이템의 시작 위치
   const itemCount = Math.floor($viewer.width() / itemWidth); // 아이템이 보이는 너비 / 아이템 하나의 너비 (버림)
   const currentPosition = $content.offset().left;
   const nextItemPosition = itemCount * itemWidth + itemWidth; //다음 아이템 위치
   var scrollTo = currentPosition - nextItemPosition;
 //  console.log(scrollTo, currentPosition, nextItemPosition);
 
-  if (-scrollTo > $content.width()) {
+  if (-scrollTo > endItemPos) {
     //그러나 전체 너비를 벗어나면 다시 초기로 되돌림.
     scrollTo = 0;
   }
