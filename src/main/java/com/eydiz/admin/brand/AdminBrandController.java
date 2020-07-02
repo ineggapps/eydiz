a553e2b7245388cdec869ed899db7c1349d41ff7
은미 <10mi33@naver.com>
2020-07-02 17:43:12 +0900

    adminBand list, read 완성

--- src/main/java/com/eydiz/admin/brand/AdminBrandController.java
+++ src/main/java/com/eydiz/admin/brand/AdminBrandController.java
@@ -1,15 +1,138 @@
 package com.eydiz.admin.brand;
 
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.eydiz.common.MyUtil;
+
 
 @Controller("brand.adminBrandController")
 @RequestMapping("/admin/brand/*")
 public class AdminBrandController {
+	@Autowired
+	private AdminBrandService service;
 	
-	@RequestMapping(value= {"/view","/"})
-	public String view() {
-		return ".adminLayout"; 
+	@Autowired
+	private MyUtil myutil;
+	
+	@RequestMapping(value= "list")
+	public String list(
+			@RequestParam(value="page", defaultValue="1") int current_page,
+			@RequestParam(defaultValue="brandName") String condition,
+			@RequestParam(defaultValue="") String keyword, 
+			Model model, HttpServletRequest req
+			) throws Exception {
+		
+		String cp = req.getContextPath();
+		
+		int rows = 10;
+		int total_page = 0;
+		int dataCount = 0;
+		
+		if(req.getMethod().equalsIgnoreCase("GET")) {
+			keyword = URLDecoder.decode(keyword, "utf-8");
+		}
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("condition", condition);
+		map.put("keyword", keyword);
+		
+		dataCount = service.dataCount(map);
+		
+		if(dataCount != 0) {
+			total_page = myutil.pageCount(rows, dataCount);
+		}
+		
+		if(total_page < current_page) {
+			current_page = total_page;
+		}
+		
+		int offset = (current_page-1) * rows;
+		if(offset < 0) offset = 0;
+		map.put("offset", offset);
+		map.put("rows", rows);
+		
+		List<Brand> list = service.listBrand(map);
+		
+		int listNum = 0;
+		int num = 0;
+		for(Brand dto : list) {
+			listNum = dataCount - (offset + num);
+			dto.setListNum(listNum);
+			num++;
+		}
+		
+		String query = "";
+		String listUrl = cp+"/admin/brand/list";
+		String readUrl = cp+"/admin/brand/read?page="+current_page;
+		
+		if(keyword.length() != 0) {
+			query = "condition=" + condition + "&keyword=" + URLEncoder.encode(keyword, "utf-8");
+		}
+		
+		if(query.length() != 0) {
+			listUrl = cp+"/admin/brand/list?query=" + query;
+			readUrl = cp+"/admin/brand/read?page=" + current_page + "&query=" + query;
+		}
+		
+		String paging = myutil.paging(current_page, total_page, listUrl);
+		
+		model.addAttribute("list", list);
+		model.addAttribute("page", current_page);
+		model.addAttribute("listUrl", listUrl);
+		model.addAttribute("condition", condition);
+		model.addAttribute("keyword", keyword);
+		model.addAttribute("paging", paging);
+		model.addAttribute("readUrl", readUrl);
+		model.addAttribute("dataCount", dataCount);
+		model.addAttribute("total_page", total_page);
+		
+		return ".adminLayout.brand.list"; 
+	}
+	
+	@RequestMapping(value="read")
+	public String read(
+			@RequestParam int brandNo,
+			@RequestParam String page,
+			@RequestParam(defaultValue="brandName") String condition,
+			@RequestParam(defaultValue="") String keyword,
+			Model model
+			) throws Exception {
+		keyword = URLDecoder.decode(keyword, "utf-8");
+		
+		String query = "page="+page;
+		if(keyword.length() != 0) {
+			query = "condition=" +condition+ "&keyword=" +keyword+ URLEncoder.encode(keyword, "utf-8");
+		}
+		
+		
+		Brand dto = service.readBrandList(brandNo);
+		if(dto == null) {
+			return "redirect:/admin/brand/list?"+query;
+		}
+		
+		Map<String, Object> map = new HashMap<>();
+		map.put("condition", condition);
+		map.put("keyword", keyword);
+		map.put("brandNo", brandNo);
+		
+		model.addAttribute("dto", dto);
+		model.addAttribute("page", page);
+		model.addAttribute("query", query);
+		
+		return ".adminLayout.brand.read";
 	}
 	
 }
+
---
+++ src/main/webapp/WEB-INF/view/admin/brand/list.jsp
@@ -0,0 +1,61 @@
+<%@ page contentType="text/html; charset=UTF-8"%>
+<%@ page trimDirectiveWhitespaces="true" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<%
+	String cp=request.getContextPath();
+%>
+
+<script type="text/javascript">
+
+function searchList() {
+	var f=document.searchForm;
+	f.submit();
+}
+
+</script>
+
+<link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+ <div class="admin_list_text">
+           <div class="titleBar">
+             <div class="title"><h2>브랜드 목록</h2></div>
+			     <form name="searchForm" action="<%=cp%>/admin/brand/list" method="post" >
+               		<div align="right">
+	                    <select name="condition">
+	                      <option value="brandName" ${condition=="brandName"?"selected='selected'":""}>브랜드</option>
+	                      <option value="managerName" ${condition=="managerName"?"selected='selected'":""}>관리자이름</option>
+	                    </select>
+                   		<input type="text" name="keyword"placeholder="검색" value="${keyword}"/>
+                    	<button type="button" class="l_searchButton" onclick="searchList()"><i class="fa fa-search" aria-hidden="true"></i></button>
+                	</div>
+				 </form>
+              </div>
+              
+
+            <div class="admin_list" >
+             <table class="admin_list_brand">
+             	<tr class="admin_list_oo">
+             		<td class="admin_list_bname"> 브랜드 이름 </td>
+					<td class="admin_list_pname">관리자 이름</td> 
+					<td class="admin_list_cname">관리자 이메일</td> 
+					<td class="admin_list_sname">관리자 전화번호</td> 
+				</tr>
+				<c:forEach var="dto" items="${list}">
+	             	<tr class="admin_list_o">
+	             		<td class="admin_list_bname"> <a href="${readUrl}&brandNo=${dto.brandNo}"> ${dto.brandName} </a></td>
+						<td class="admin_list_pname">${dto.managerName}</td> 
+						<td class="admin_list_cname">${dto.managerEmail}</td> 
+						<td class="admin_list_sname">${dto.managerPhone}</td> 
+					</tr>
+				</c:forEach>
+			 </table>
+		    </div>
+
+           </div>
+ 		<table style="width: 1100px; border-spacing: 0px;">
+			<tr height="35">
+				<td align="center">
+					 ${dataCount==0?"데이터가 없습니다.":paging}
+				</td>
+			</tr>
+		</table>
