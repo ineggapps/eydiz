4972f981232826c1ab976af753082e532780f029
hanjunik <hanjunik@gmail.com>
2020-07-03 17:49:56 +0900

    guide 수정, school css수정

---
+++ src/main/java/com/eydiz/school/SchoolServiceImpl.java
@@ -0,0 +1,13 @@
+package com.eydiz.school;
+
+import org.springframework.stereotype.Service;
+
+@Service("school.schoolService")
+public class SchoolServiceImpl implements SchoolService{
+
+	@Override
+	public void test() {
+		
+	}
+
+}
