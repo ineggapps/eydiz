ad22d967d2c308b3dd0322416bdddc6ad740fc42
inegg <inegg.apps@gmail.com>
2020-07-08 17:26:07 +0900

    프로젝트 새소식 조회 페이지 작성 (더보기 버튼)

--- src/main/java/com/eydiz/main/MainConstant.java
+++ src/main/java/com/eydiz/main/MainConstant.java
@@ -6,11 +6,6 @@ public interface MainConstant {
 	//ATTRIBUTE
 	public static final String ATTRIBUTE_FUNDING_CATEGORY = "fundingCategory";
 	public static final String ATTRIBUTE_PROJECT = "project";
-	public static final String ATTRIBUTE_COUNT = "count";
-	public static final String ATTRIBUTE_PAGE = "page";
-	public static final String ATTRIBUTE_OFFSET = "offset";
-	public static final String ATTRIBUTE_ROWS = "rows";
-	public static final String ATTRIBUTE_PAGE_COUNT = "page_count";
 	public static final String ATTRIBUTE_CATEGORYNO = "categoryNo";
 	public static final String ATTRIBUTE_CATEGORY_NAME = "categoryName";
 }
